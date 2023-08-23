ab82fe11a19e8586a81933303105d94e5e1aaf34
start.vaadin.com <no-reply@vaadin.com>
2023-08-23 04:28:04 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/inglogingenerator/INGLoginGeneratorView.java
@@ -0,0 +1,33 @@
+package com.example.application.views.inglogingenerator;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("ING Login Generator")
+@Route(value = "", layout = MainLayout.class)
+public class INGLoginGeneratorView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public INGLoginGeneratorView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
